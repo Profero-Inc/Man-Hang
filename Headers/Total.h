46b0c4ecdfc6dc7ab0076f1894e387abe514bcf9
Mohamed Hazem <M.Hazem2452@nu.edu.eg>
2025-04-28 22:24:45 +0300

    Update Total.h

--- Headers/Total.h
+++ Headers/Total.h
@@ -1,6 +1,6 @@
-#ifndef TOTAL_H_INCLUDED
-#define TOTAL_H_INCLUDED
+#ifndef TOTAL_H_INCLUDED    // Check if TOTAL_H_INCLUDED is already defined
+#define TOTAL_H_INCLUDED    // Define TOTAL_H_INCLUDED to avoid double inclusion
 
-void drawgameui();
+void drawgameui();           // Declare the drawgameui function
 
-#endif
+#endif                       // End of ifndef
